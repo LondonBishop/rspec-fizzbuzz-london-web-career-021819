11a480536cffa8ccbee3f5a8dceae4a2d9c85703
HARRY BHATT <bob@brahmingate.co.uk>
2019-02-12 13:36:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(numberIn)
   case numberIn
-  when (numberIn % 3 == 0 && numberIn % 5 == 0)
-    retrun "FizzBuzz"
-  when (numberIn % 3 == 0)
-    return "Fizz"
-  when (numberIn % 5 == 0)
-    return "Buzz"
-  end
+    when (numberIn % 3 == 0 && numberIn % 5 == 0)
+      retrun "FizzBuzz"
+    when (numberIn % 3 == 0)
+      return "Fizz"
+    when (numberIn % 5 == 0)
+      return "Buzz"
+    end
 end
 
 # Don't forget! This file needs to be 'required' in its spec file
