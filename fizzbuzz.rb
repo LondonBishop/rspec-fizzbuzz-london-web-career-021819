3b4e16a8d36022cad68bbd5b43f35de56c676fe6
HARRY BHATT <bob@brahmingate.co.uk>
2019-02-12 13:50:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,14 @@
 def fizzbuzz(numberIn)
-  case numberIn
-    when (numberIn % 3 == 0 && numberIn % 5 == 0) === true
+  if (numberIn % 3 == 0 && numberIn % 5 == 0)
       return "FizzBuzz"
-    when (numberIn % 3 == 0) === true
-      return "Fizz"
-    when (numberIn % 5 == 0) === true
-      return "Buzz"
-    end
-end
+    elsif (numberIn % 3 == 0)
+        return "Fizz"
+      elsif (numberIn % 5 == 0)
+         return "Buzz"
+       end
+     end
+   end
+ end
 
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
