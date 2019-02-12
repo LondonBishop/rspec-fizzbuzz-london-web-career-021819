4946aa3f3680c37dab86506467d5e43d9db586ac
HARRY BHATT <bob@brahmingate.co.uk>
2019-02-12 13:56:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,5 @@ def fizzbuzz(numberIn)
   return "Fizz" if (numberIn % 3 == 0)
   return "Buzz" if (numberIn % 5 == 0)
 end
- 
-
-# Don't forget! This file needs to be 'required' in its spec file
+ # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do thi
